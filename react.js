6c1507e0ec00c9d116944677667eff1e8d93251d
moniruzzaman335 <dev.moniruzzaman@gmail.com>
2024-10-04 19:18:08 +0600

    test react code successfully

--- react.js
+++ react.js
@@ -1,2 +1,14 @@
-const root = document.getElementById("root");
+// this is normal way to create element
+// const element = React.createElement("div", null, "Start Learning React");
 
+const element = (
+  <div>
+        <h1>
+            Welcome to learn react JS
+        </h1>
+  </div>  
+);
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(element);
